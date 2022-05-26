4639b47e95d83ab2bfa1a0ea53021babcb0e0dcb
rachel <rachlin232@gmail.com>
2022-05-26 14:02:20 -0500

    trying other methods

---
+++ dates.py
@@ -0,0 +1,11 @@
+# advanced search template:
+# https://twitter.com/search?q=thinspo%20until%3A2015-01-31%20since%3A2015-01-01&src=typed_query
+# https://twitter.com/search?q=thinspo%20until%3AYYYY-MM-DD%20since%3AYYYY-MM-DD&src=typed_query
+
+# create dict for all the date ranges??
+date_ranges = [
+    {
+        "since": "2015-01-01",
+        "to": "2015-01-31"
+    }
+]
--- index.py
+++ index.py
@@ -1,24 +1,46 @@
 from selenium import webdriver
+#from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
+from dates import date_ranges
+
+import time
+
 driver = webdriver.Chrome("chromedriver.exe")
+#driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
 
-driver.get('https://twitter.com')
+# TODO: don't need to login to get tweets!!!
 
-def login (username, password):
-    # enter username
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='username']")))
-    username_field = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[2]/div/label/input')
-    username_field.click()
-    username_field.send_keys(username)
+#driver.get('https://twitter.com/search?q=thinspo')
 
-    # enter password
-    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.CSS_SELECTOR, "input[name='password']")))
-    password_field = driver.find_element_by_xpath('//*[@id="react-root"]/section/main/article/div[2]/div[1]/div/form/div[3]/div/label/input')
-    password_field.click()
-    password_field.send_keys(password)
-    password_field.send_keys(Keys.RETURN) # enter to login
+# 1. count tweets by time 
+
+
+# TESTING
+driver.get('https://twitter.com/search?q=thinspo%20until%3A2015-01-02%20since%3A2015-01-01&src=typed_query')
+count = 0
+# WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div[6]/div/div/div/article/div/div/div/div[2]')))
+# post = driver.find_element_by_xpath('//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div/div[6]/div/div/div/article/div/div/div/div[2]')
+# post.click()
+
+# timeline container:
+# //*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div
+
+tl_xpath = '//*[@id="react-root"]/div/div/div[2]/main/div/div/div/div[1]/div/div[2]/div/section/div/div'
+WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, tl_xpath)))
+parent_div = driver.find_element_by_xpath(tl_xpath)
+count_of_divs = len(parent_div.find_elements_by_xpath("./div"))
+print(count_of_divs)
+
+time.sleep(1)
+body = driver.find_elements_by_tag_name('body')
+for _ in range(100):
+   body.send.keys(Keys.PAGE_DONW)
+   time.sleep(0.2)
+tweets = driver.find_elements_by_class_name('tweet-text')
+for tweet in tweets:
+    print(tweet.text)
